18e44ba8786ccdfdce3afeae7ec0c4c8ec17a413
Breno Valadaes <lucas3820@bol.com.br>
2019-10-19 11:37:30 -0300

    Biblioteca com funçoes de fatorial e primo

---
+++ matematica.py
@@ -0,0 +1,18 @@
+# fatorial
+def fatorial(numero):
+    fat = 1
+    while numero > 0:
+        fat = fat * numero
+        numero = numero - 1
+    return fat
+def primo(numero):
+    div = 0
+    valor = 1
+    while valor <= numero:
+        if numero%valor == 0:
+            div = div + 1
+        valor = valor + 1
+    if div == 2:
+        print("É primo")
+    else:
+        print("Não é primo")
---
+++ user.py
@@ -0,0 +1,6 @@
+import matematica as m
+
+numero = int(input("Informe um número: "))
+resposta = m.fatorial(numero)
+print(resposta)
+m.primo(numero)
